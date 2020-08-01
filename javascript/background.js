cd82aa27e3987ae00d2dd898a844c67aa24ed85d
adamwexler01 <adamwexler01@ufl.edu>
2020-07-31 22:21:16 -0400

    adding fetch request to service

--- javascript/background.js
+++ javascript/background.js
@@ -14,25 +14,33 @@ chrome.runtime.onInstalled.addListener(function() {
 
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
+chrome.contextMenus.onClicked.addListener(async function(info, tab){
+
   switch(info.menuItemId){
     case "check_policy":
 
       /* add url below for where to send the request */
-      let url = info.linkUrl
+      const url = info.linkUrl
+
+      /* add url below for where the tos will be sent */
+      const baseUrl = 'http://localhost:5000/';
+      const tagUrl = `${baseUrl}/tags/common`
 
       if(url == null){
         console.log('Not policy is selected')
         return
       }
 
-      let response = fetch(url)
+      /* We are only collecting paragraph tags */
+      /* Need to think about a better solution for finding content in tos */
+      const response = fetch(url)
             .then(response => response.text())
             .then(data => {
               // We are receiving text
               // Transforming to html
               var parser = new DOMParser();
               var htmlDoc = parser.parseFromString(data, 'text/html');
+              console.log(htmlDoc);
               let pTags = htmlDoc.getElementsByTagName('p')
               let pList = []
 
@@ -40,18 +48,23 @@ chrome.contextMenus.onClicked.addListener(function(info, tab){
                 pList.push(p.innerText)
               }
 
+              console.log(pList) //Console log used for debugging
               return pList
             });
 
-      /* Convert the legal document to an object */
-      let request = {
-        content: response
-      }
-
-      console.log(request)
-
 
       /* Send the legal document to the service */
+      /* Start by preparing the headers & body */
+      let headers = {'Content-Type': 'application/json', 'Origin': 'http://localhost:5000'}
+      let body = {content: await response}
+
+      /* Need to setup debug logger to prevent pii info from spilling */
+      console.log("headers: " + JSON.stringify(headers))
+      console.log("body: " + JSON.stringify(body))
+
+      /* Send the content from tos to service */
+      fetch(tagUrl, {method: 'POST', headers: headers, body: JSON.stringify(body)})
+            .then(response => console.log(response));
 
       break;
   }
